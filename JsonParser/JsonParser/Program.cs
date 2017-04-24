bdf6291e32b5b59d0361fadb9d3b89ad6643be51
Yuriy Fedenko <f.yura.o@gmail.com>
2017-04-24 19:55:38 +0300

    test parsing

--- JsonParser/JsonParser/Program.cs
+++ JsonParser/JsonParser/Program.cs
@@ -10,9 +10,13 @@ namespace JsonParser
     {
         static void Main(string[] args)
         {
+            
             FileUtils file = new FileUtils();
             string test = file.readUsers();
             Console.WriteLine(test);
+            var userColections = UserSingleton.getInstance(test);
+            Console.WriteLine(userColections.Users.UserCollection[0].FirstName);
+            Console.WriteLine(userColections.Users.UserCollection[1].FirstName);
             Console.Read();
         }
     }
